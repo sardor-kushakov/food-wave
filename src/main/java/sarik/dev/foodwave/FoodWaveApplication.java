dea945ce76bd25fa3c7b1df79b862f7d6982787b
sardor-kushakov <sardorkushakov.uz@gmail.com>
2024-11-03 01:47:32 +0500

    project created

---
+++ src/main/java/sarik/dev/foodwave/FoodWaveApplication.java
@@ -0,0 +1,13 @@
+package sarik.dev.foodwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodWaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodWaveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sarik/dev/foodwave/FoodWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package sarik.dev.foodwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodWaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
